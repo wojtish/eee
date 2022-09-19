c217be56afcc583b4091101d97ba2645eb80ce39
wojtish <sikorawojtek@o2.pl>
2022-09-19 06:14:28 +0200

    Update function memento.js

--- function memento.js
+++ function memento.js
@@ -1,6 +1,9 @@
 function memento(t){
 if ( t == true )
 {
+var linkt = "tasker://secondary?text="
+var intview = intent("android.intent.action.VIEW");
+
 intview.data(linkt + "memento")
 intview.send()
 //message("test ok");
