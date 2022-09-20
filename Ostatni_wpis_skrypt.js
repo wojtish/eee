af1e605a7a2439f95339ddc2ee4f2cd5fbf1a092
wojtish <sikorawojtek@o2.pl>
2022-09-20 15:00:19 +0200

    Create Ostatni_wpis_skrypt.js

---
+++ Ostatni_wpis_skrypt.js
@@ -0,0 +1,13 @@
+var entri = lib().entries(); 
+var l = entri.lenght
+var n = "ostatni wpis"
+var z = entri[0].field("ostatni wpis")
+var q = parseInt(z);
+
+entri[z].show();
+
+for (var ent = 0; ent < entri.length; ent++)
+ 
+{
+entri[ent].set(n,q+1); 
+}
