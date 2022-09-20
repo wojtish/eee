f921006cbdab76dbe23956b5f75b10c58d56065b
wojtish <sikorawojtek@o2.pl>
2022-09-20 14:41:40 +0200

    Create function_ostatni_wpis.js

---
+++ function_ostatni_wpis.js
@@ -0,0 +1,16 @@
+function(ostwpis)
+if (ostwpis = true)
+var entri = lib().entries(); 
+var l = entri.lenght
+var n = "ostatni wpis"
+var z = entri[0].field("ostatni wpis")
+var q = parseInt(z);
+
+entri[z].show();
+for (var ent = 0; ent < entri.length; ent++)
+ 
+{
+entri[ent].set(n,q+1); 
+
+}
+
