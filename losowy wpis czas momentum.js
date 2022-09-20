edcc606b026612e10bfc9897c49f5008cb2a1b7f
wojtish <sikorawojtek@o2.pl>
2022-09-20 21:32:36 +0200

    Update losowy wpis czas momentum.js

--- losowy wpis czas momentum.js
+++ losowy wpis czas momentum.js
@@ -3,6 +3,12 @@ var entries = lib().entries()
 var min = Math.ceil(10000);
 var max = Math.floor(1000);
 
+var order = { compare: function(a,b) { return a.field("czas momentum") - b.field("czas momentum"); }}
+
+entries.sort(order)
+entries.reverse()[0].show()
+//entries[0].show()
+
 for (var ent = 0; ent < entries.length; ent++){    
 var ggg = Math.floor(Math.random() * (max - min)) + min
 var jjj = ggg / 100
@@ -10,9 +16,3 @@ var kkk = jjj.toFixed(2)
 
 entries[ent].set("czas momentum",kkk);
 }
-
-var order = { compare: function(a,b) { return a.field("czas momentum") - b.field("czas momentum"); }}
-
-entries.sort(order)
-
-entries[0].show()
