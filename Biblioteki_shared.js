8e3c8b7b5264a499797bc2b319b15e8795b89906
wojtish <sikorawojtek@o2.pl>
2022-09-25 16:33:03 +0200

    Create Biblioteki_shared.js

---
+++ Biblioteki_shared.js
@@ -0,0 +1,27 @@
+var linkt = "tasker://secondary?text="
+var intview = intent("android.intent.action.VIEW");
+
+
+var hp = "Haczów przestrzenie"
+var hpbn = libByName(hp)
+var zt = "zadania terminarz zajęć prac zadań zadania"
+var ztbn = libByName(zt)
+var hk = "Haczów katalog"
+var hkbn = libByName(hk)
+var ma = "metalowe artykuły"
+var mabn = libByName(ma)
+var tc = "techniczna"
+var tcbn = libByName(tc)
+var ps = "PRODUKTY SPOŻYWCZE"
+var psbn = libByName(ps)
+var rt = "RUTYNY"
+var rtbn = libByName(rt)
+var an = "angielski"
+var anbn = libByName(an)
+var lw = "ludzie w miłości kontaktujący się"
+var lwbn = libByName(lw)
+var pr  = "przepisy"
+var prbn  = libByName(pr)
+// var  = ""
+// var  = ""
+// var  = ""
