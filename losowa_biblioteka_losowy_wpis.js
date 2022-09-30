0d18d7f37d3bae3f981c2243841daa8744c4d347
wojtish <sikorawojtek@o2.pl>
2022-09-30 19:23:48 +0200

    Update losowa_biblioteka_losowy_wpis.js

--- losowa_biblioteka_losowy_wpis.js
+++ losowa_biblioteka_losowy_wpis.js
@@ -1,4 +1,4 @@
-var libraries = [lk,hk,hp,zt,lw,ma,rt,tc,ps,pr,sc]
+var libraries = [an,sm,lk,hk,hp,zt,lw,ma,rt,tc,ps,pr,sc]
 
 var item = libraries[Math.floor(Math.random()*libraries.length)];
 message(item)
