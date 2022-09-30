475a5636bf27c38da876d1436c2635c9241869f7
wojtish <sikorawojtek@o2.pl>
2022-09-30 19:02:41 +0200

    Create losowa_biblioteka_losowy_wpis.js

---
+++ losowa_biblioteka_losowy_wpis.js
@@ -0,0 +1,23 @@
+var libraries = [hk,hp,zt,lw,ma,rt,tc,ps,pr,sc]
+
+var item = libraries[Math.floor(Math.random()*libraries.length)];
+message(item)
+
+var entries = libByName(item).entries()
+
+var min = Math.ceil(10000);
+var max = Math.floor(1000);
+
+var order = { compare: function(a,b) { return a.field("czas momentum") - b.field("czas momentum"); }}
+
+entries.sort(order)
+entries.reverse()[0].show()
+//entries[0].show()
+
+for (var ent = 0; ent < entries.length; ent++){    
+var ggg = Math.floor(Math.random() * (max - min)) + min
+var jjj = ggg / 100
+var kkk = jjj.toFixed(2)
+
+entries[ent].set("czas momentum",kkk);
+}
