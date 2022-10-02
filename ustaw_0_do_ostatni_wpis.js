c45a02873862f2af0f9bc9c24daffae9a4bd7876
wojtish <sikorawojtek@o2.pl>
2022-10-02 18:10:31 +0200

    Update ustaw_0_do_ostatni_wpis.js

--- ustaw_0_do_ostatni_wpis.js
+++ ustaw_0_do_ostatni_wpis.js
@@ -11,11 +11,11 @@ entri[ent].set(n,0); 
 
 }
 
-entri[z].show();
-for (var ent = 0; ent < entri.length; ent++)
+//entri[z].show();
+//for (var ent = 0; ent < entri.length; ent++)
  
-{
-entri[ent].set(n,q); 
+//{
+//entri[ent].set(n,q); 
 
-}
+//}
 
