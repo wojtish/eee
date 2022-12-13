42c29022ae7b39b2364ed34056140a0c41fe43bf
wojtish <20014605+wojtish@users.noreply.github.com>
2022-12-13 22:16:42 +0100

    Create Zza biblioteki shared.js

---
+++ Zza biblioteki shared.js
@@ -0,0 +1,55 @@
+var linkt = "tasker://secondary?text="
+var intview = intent("android.intent.action.VIEW");
+
+
+var hp = "Haczów przestrzenie"
+var hpbn = libByName(hp)
+var zt = "zadania terminarz zajęć prac zadań zadania"
+var ztbn = libByName(zt)
+var hk = "Haczów katalog"
+var hkbn = libByName(hk)
+var ma = "metalowe artykuły"
+var mabn = libByName(ma)
+var tc = "techniczna"
+var tcbn = libByName(tc)
+var ps = "PRODUKTY SPOŻYWCZE"
+var psbn = libByName(ps)
+var rt = "RUTYNY"
+var rtbn = libByName(rt)
+var an = "angielski"
+var anbn = libByName(an)
+var lw = "ludzie w miłości kontaktujący się"
+var lwbn = libByName(lw)
+var pr  = "przepisy"
+var prbn  = libByName(pr)
+var lk = "lektury"
+var lkbn  = libByName(lk)
+var sm = "samochód"
+var smbn = libByName(sm)
+var sc = "sukcesy cuda"
+var scbn = libByName(sc)
+var gc = "garderoba ubrania ciuchy"
+var gcbn = libByName(gc)
+var pp = "plan posiłków"
+var ppbn = libByName(pp)
+var bw = "budżet wydatki przychody zarządzanie"
+var bwbn = libByName(bw)
+var cf = "ćwiczenia fitness youtube musepic"
+var cfbn = libByName(cf)
+var ag = "agenda memento harmonogram terminarz kalendarz zadania"
+var agbn = libByName(ag)
+var zb = "życie ukryte w Bogu Ojcu"
+var zbbn =libByName(zb)
+var mo = "mój ogród"
+var mobn = libByName(mo)
+var wi = "wiara"
+var wibn = libByName(wi)
+var tm = "tabliczka mnożenia"
+var tmbn = libByName(tm)
+var tt = "time tracking"
+var ttbn = libByName(tt)
+var lj = "lokalizacje adresy"
+var ljbn = libByName(lj)
+
+
+
