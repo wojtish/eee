d8e927312a9402f732a2ea0230759080dea20846
wojtish <20014605+wojtish@users.noreply.github.com>
2022-12-14 08:40:26 +0100

    Update haha.js

--- haha.js
+++ haha.js
@@ -1,7 +1,7 @@
 var libraries = [tm,tt,lj,wi,mo,zb,ag,cf,bw,pp,gc,an,sm,lk,hk,hp,zt,lw,ma,rt,tc,ps,pr,sc]
 //var libraries = [ag,cf]
 var item = libraries[Math.floor(Math.random()*libraries.length)];
-message(item)
+//message(item)
 
 var entries = libByName(item).entries()
 
