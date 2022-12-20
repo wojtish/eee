ddd66fe96bb138bf3a0863d3c81de3b3b703df07
wojtish <20014605+wojtish@users.noreply.github.com>
2022-12-20 12:39:46 +0100

    Update losowa_biblioteka_losowy_wpis.js

--- losowa_biblioteka_losowy_wpis.js
+++ losowa_biblioteka_losowy_wpis.js
@@ -1,4 +1,4 @@
-var libraries = [pt,sp,ic,wb,rk,tm,tt,lj,wi,mo,zb,ag,cf,bw,pp,gc,an,sm,lk,hk,hp,zt,lw,ma,rt,tc,ps,pr,sc];
+var libraries = [kk,pt,sp,ic,wb,rk,tm,tt,lj,wi,mo,zb,ag,cf,bw,pp,gc,an,sm,lk,hk,hp,zt,lw,ma,rt,tc,ps,pr,sc];
 //var libraries = [ag,cf];
 var item = libraries[Math.floor(Math.random()*libraries.length)];
 //message(item)
