7c5ad735b47f805a55be989e536b2293cab8d9b1
wojtish <20014605+wojtish@users.noreply.github.com>
2022-12-20 12:28:39 +0100

    Update losowa_biblioteka_losowy_wpis.js

--- losowa_biblioteka_losowy_wpis.js
+++ losowa_biblioteka_losowy_wpis.js
@@ -3,21 +3,21 @@ var libraries = [pt,sp,ic,wb,rk,tm,tt,lj,wi,mo,zb,ag,cf,bw,pp,gc,an,sm,lk,hk,hp,
 var item = libraries[Math.floor(Math.random()*libraries.length)];
 //message(item)
 
-var entries = libByName(item).entries()
+var entries = libByName(item).entries();
 
 var min = Math.ceil(10000);
 var max = Math.floor(1000);
 
 var order = { compare: function(a,b) { return a.field("czas momentum") - b.field("czas momentum"); }}
 
-entries.sort(order)
-entries.reverse()[0].show()
-//entries[0].show()
+entries.sort(order);
+entries.reverse()[0].show();
+//entries[0].show();
 
 for (var ent = 0; ent < entries.length; ent++){    
-var ggg = Math.floor(Math.random() * (max - min)) + min
-var jjj = ggg / 100
-var kkk = jjj.toFixed(2)
+var ggg = Math.floor(Math.random() * (max - min)) + min;
+var jjj = ggg / 100 ;
+var kkk = jjj.toFixed(2);
 
 entries[ent].set("czas momentum",kkk);
 }
