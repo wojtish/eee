383cb749ccc538f1a732799a7cc9bc01d8bf9ed2
wojtish <20014605+wojtish@users.noreply.github.com>
2022-12-23 07:32:04 +0100

    Create losowy_wpis_h_inside.js

---
+++ losowy_wpis_h_inside.js
@@ -0,0 +1,335 @@
+var entries = lib().entries()
+
+var min = Math.ceil(10000);
+var max = Math.floor(1000);
+var his = entry().field("h")
+
+
+var order = { compare: function(a,b) { return a.field("czas momentum") - b.field("czas momentum"); }}
+
+
+entries.sort(order)
+var entry1 = entries.reverse()[0]
+var entry2 = entries.reverse()[1]
+
+var entry3 = entries.reverse()[2]
+var entry4 = entries.reverse()[3]
+var entry5 = entries.reverse()[4]
+var entry6 = entries.reverse()[5]
+
+var entry7 = entries.reverse()[6]
+
+var entry8 = entries.reverse()[7]
+
+var entry9 = entries.reverse()[8]
+
+var entry10 = entries.reverse()[9]
+var entry11 = entries.reverse()[10]
+var entry12 = entries.reverse()[11]
+
+
+
+
+var entry13 = entries.reverse()[12]
+var entry14 = entries.reverse()[13]
+var entry15 = entries.reverse()[14]
+var entry16 = entries.reverse()[15]
+
+var entry17 = entries.reverse()[16]
+
+var entry18 = entries.reverse()[17]
+
+var entry19 = entries.reverse()[18]
+
+var entry20 = entries.reverse()[19]
+var entry21 = entries.reverse()[20]
+var entry22 = entries.reverse()[21]
+
+
+
+var entry23 = entries.reverse()[22]
+var entry24 = entries.reverse()[23]
+
+
+var entry25 = entries.reverse()[24]
+var entry26 = entries.reverse()[25]
+var entry27 = entries.reverse()[26]
+var entry28 = entries.reverse()[27]
+
+
+var entry29 = entries.reverse()[28]
+
+
+var entry30 = entries.reverse()[29]
+
+var entry31 = entries.reverse()[30]
+var entry32 = entries.reverse()[31]
+var entry33 = entries.reverse()[32]
+var entry34 = entries.reverse()[33]
+var entry35 = entries.reverse()[34]
+var entry36 = entries.reverse()[35]
+var entry37 = entries.reverse()[36]
+var entry38 = entries.reverse()[37]
+var entry39 = entries.reverse()[38]
+var entry40 = entries.reverse()[39]
+
+var entry41 = entries.reverse()[40]
+
+var entry42 = entries.reverse()[41]
+
+var entry43 = entries.reverse()[42]
+
+var entry44 = entries.reverse()[43]
+
+var entry45 = entries.reverse()[44]
+
+var entry46 = entries.reverse()[45]
+
+var entry47 = entries.reverse()[46]
+
+var entry48 = entries.reverse()[47]
+
+var entry49 = entries.reverse()[48]
+
+var entry50 = entries.reverse()[49]
+
+
+
+if (entry1.field("h") == 0){
+entry1.set("h",1)
+entries.reverse()[0].show()
+
+}
+else if (entry2.field("h") == 0){
+entry2.set('h',1)
+entries.reverse()[1].show()
+}
+else if (entry3.field("h") == 0){
+entry3.set("h",1)
+entries.reverse()[2].show()
+}
+else if (entry4.field("h") == 0){
+entry4.set("h",1)
+entries.reverse()[3].show()
+}
+else if (entry5.field("h") == 0){
+entry5.set('h',1)
+entries.reverse()[4].show()
+}
+
+else if (entry6.field("h") == 0){
+entry6.set('h',1)
+entries.reverse()[5].show()
+}
+
+else if (entry7.field("h") == 0){
+entry7.set('h',1)
+entries.reverse()[6].show()
+}
+else if (entry8.field("h") == 0){
+entry8.set('h',1)
+entries.reverse()[7].show()
+}
+else if (entry9.field("h") == 0){
+entry9.set('h',1)
+entries.reverse()[8].show()
+}
+
+else if (entry10.field("h") == 0){
+entry10.set('h',1)
+entries.reverse()[9].show()
+}
+
+else if (entry11.field("h") == 0){
+entry11.set('h',1)
+entries.reverse()[10].show()
+}
+else if (entry12.field("h") == 0){
+entry12.set('h',1)
+entries.reverse()[11].show()
+}
+
+
+else if (entry13.field("h") == 0){
+entry13.set('h',1)
+entries.reverse()[12].show()
+}
+else if (entry14.field("h") == 0){
+entry14.set('h',1)
+entries.reverse()[13].show()
+}
+else if (entry15.field("h") == 0){
+entry15.set('h',1)
+entries.reverse()[14].show()
+}
+
+else if (entry16.field("h") == 0){
+entry16.set('h',1)
+entries.reverse()[15].show()
+}
+
+else if (entry17.field("h") == 0){
+entry17.set('h',1)
+entries.reverse()[16].show()
+}
+else if (entry18.field("h") == 0){
+entry18.set('h',1)
+entries.reverse()[17].show()
+}
+else if (entry19.field("h") == 0){
+entry19.set('h',1)
+entries.reverse()[18].show()
+}
+
+else if (entry20.field("h") == 0){
+entry20.set('h',1)
+entries.reverse()[19].show()
+}
+
+else if (entry21.field("h") == 0){
+entry21.set('h',1)
+entries.reverse()[20].show()
+}
+else if (entry22.field("h") == 0){
+entry22.set('h',1)
+entries.reverse()[21].show()
+}
+
+
+else if (entry23.field("h") == 0){
+entry23.set('h',1)
+entries.reverse()[22].show()
+}
+else if (entry24.field("h") == 0){
+entry24.set('h',1)
+entries.reverse()[23].show()
+}
+else if (entry25.field("h") == 0){
+entry25.set('h',1)
+entries.reverse()[24].show()
+}
+
+else if (entry26.field("h") == 0){
+entry26.set('h',1)
+entries.reverse()[25].show()
+}
+
+else if (entry27.field("h") == 0){
+entry27.set('h',1)
+entries.reverse()[26].show()
+}
+else if (entry28.field("h") == 0){
+entry28.set('h',1)
+entries.reverse()[27].show()
+}
+else if (entry29.field("h") == 0){
+entry29.set('h',1)
+entries.reverse()[28].show()
+}
+else if (entry30.field("h") == 0){
+entry30.set('h',1)
+entries.reverse()[29].show()
+}
+
+else if (entry31.field("h") == 0){
+entry31.set('h',1)
+entries.reverse()[30].show()
+}
+else if (entry32.field("h") == 0){
+entry32.set('h',1)
+entries.reverse()[31].show()
+}
+
+else if (entry33.field("h") == 0){
+entry33.set('h',1)
+entries.reverse()[32].show()
+}
+else if (entry34.field("h") == 0){
+entry34.set('h',1)
+entries.reverse()[33].show()
+}
+else if (entry35.field("h") == 0){
+entry35.set('h',1)
+entries.reverse()[34].show()
+}
+
+else if (entry36.field("h") == 0){
+entry36.set('h',1)
+entries.reverse()[35].show()
+}
+
+else if (entry37.field("h") == 0){
+entry37.set('h',1)
+entries.reverse()[36].show()
+}
+else if (entry38.field("h") == 0){
+entry38.set('h',1)
+entries.reverse()[37].show()
+}
+else if (entry39.field("h") == 0){
+entry39.set('h',1)
+entries.reverse()[38].show()
+}
+
+else if (entry40.field("h") == 0){
+entry40.set('h',1)
+entries.reverse()[39].show()
+}
+
+else if (entry41.field("h") == 0){
+entry41.set('h',1)
+entries.reverse()[40].show()
+}
+else if (entry42.field("h") == 0){
+entry42.set('h',1)
+entries.reverse()[41].show()
+}
+
+
+else if (entry43.field("h") == 0){
+entry43.set('h',1)
+entries.reverse()[42].show()
+}
+else if (entry44.field("h") == 0){
+entry44.set('h',1)
+entries.reverse()[43].show()
+}
+else if (entry45.field("h") == 0){
+entry45.set('h',1)
+entries.reverse()[44].show()
+}
+
+else if (entry46.field("h") == 0){
+entry46.set('h',1)
+entries.reverse()[45].show()
+}
+
+else if (entry47.field("h") == 0){
+entry47.set('h',1)
+entries.reverse()[46].show()
+}
+else if (entry48.field("h") == 0){
+entry48.set('h',1)
+entries.reverse()[47].show()
+}
+else if (entry49.field("h") == 0){
+entry49.set('h',1)
+entries.reverse()[48].show()
+}
+
+
+//entries[0].show()
+
+//entries.reverse()[0].show()
+//entries[0].show()
+
+for (var ent = 0; ent < entries.length; ent++){    
+var ggg = Math.floor(Math.random() * (max - min)) + min
+var jjj = ggg / 100
+var kkk = jjj.toFixed(2)
+
+entries[ent].set("czas momentum",kkk);
+}
+
+
+
