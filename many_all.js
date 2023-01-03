d1e0eae56a494cf8494755fc4608b0c491e1f284
wojtish <20014605+wojtish@users.noreply.github.com>
2023-01-03 11:45:41 +0100

    Update many_all.js

--- many_all.js
+++ many_all.js
@@ -1,11 +1,11 @@
 //script many all
 var sum = 0
-var libclients = lib() // Rename this library
-var entries = libclients.entries() // Load all entries
-var l = entries.length; // Number of entries
-for (var j=0;j<l;j++){ // loop
-var m =entries[j].field('many') // entry j . Feldneme Intex
+var libclients = lib()
+var entries = libclients.entries()
+var l = entries.length
+for (var j=0;j<l;j++){
+var m = entries[j].field('many') 
 var sum = sum + m
 };
-//message (sum) // Results
+//message (sum) 
 entry().set("many all",sum);
