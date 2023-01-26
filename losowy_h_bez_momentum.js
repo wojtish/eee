e1c951a75b5c3cfeea06b32220b457bcd2f1eb59
wojtish <20014605+wojtish@users.noreply.github.com>
2023-01-26 11:17:59 +0100

    Create losowy_h_bez_momentum.js

---
+++ losowy_h_bez_momentum.js
@@ -0,0 +1,268 @@
+var entries = lib().entries()
+
+var min = Math.ceil(10000);
+var max = Math.floor(1000);
+var his = entry().field("h")
+
+
+var order = { compare: function(a,b) { return a.field("czas momentum") - b.field("czas momentum"); }}
+
+
+entries.sort(order)
+var entry1 = entries.reverse()[0]
+var entry2 = entries.reverse()[1]
+var entry3 = entries.reverse()[2]
+var entry4 = entries.reverse()[3]
+var entry5 = entries.reverse()[4]
+var entry6 = entries.reverse()[5]
+var entry7 = entries.reverse()[6]
+var entry8 = entries.reverse()[7]
+var entry9 = entries.reverse()[8]
+var entry10 = entries.reverse()[9]
+var entry11 = entries.reverse()[10]
+var entry12 = entries.reverse()[11]
+var entry13 = entries.reverse()[12]
+var entry14 = entries.reverse()[13]
+var entry15 = entries.reverse()[14]
+var entry16 = entries.reverse()[15]
+var entry17 = entries.reverse()[16]
+var entry18 = entries.reverse()[17]
+var entry19 = entries.reverse()[18]
+var entry20 = entries.reverse()[19]
+var entry21 = entries.reverse()[20]
+var entry22 = entries.reverse()[21]
+var entry23 = entries.reverse()[22]
+var entry24 = entries.reverse()[23]
+var entry25 = entries.reverse()[24]
+var entry26 = entries.reverse()[25]
+var entry27 = entries.reverse()[26]
+var entry28 = entries.reverse()[27]
+var entry29 = entries.reverse()[28]
+var entry30 = entries.reverse()[29]
+var entry31 = entries.reverse()[30]
+var entry32 = entries.reverse()[31]
+var entry33 = entries.reverse()[32]
+var entry34 = entries.reverse()[33]
+var entry35 = entries.reverse()[34]
+var entry36 = entries.reverse()[35]
+var entry37 = entries.reverse()[36]
+var entry38 = entries.reverse()[37]
+var entry39 = entries.reverse()[38]
+var entry40 = entries.reverse()[39]
+var entry41 = entries.reverse()[40]
+var entry42 = entries.reverse()[41]
+var entry43 = entries.reverse()[42]
+var entry44 = entries.reverse()[43]
+var entry45 = entries.reverse()[44]
+var entry46 = entries.reverse()[45]
+var entry47 = entries.reverse()[46]
+var entry48 = entries.reverse()[47]
+var entry49 = entries.reverse()[48]
+var entry50 = entries.reverse()[49]
+
+
+if (entry1.field("h") == 0 && entry1.field("wykluczone") != 1){
+entry1.set("h",1)
+entries.reverse()[0].show()
+//************"************
+}
+else if (entry2 != null && entry2.field("h") == 0 && entry2.field("wykluczone") != 1){
+entry2.set('h',1)
+entries.reverse()[1].show()
+}
+else if (entry3 != null && entry3.field("h") == 0 && entry3.field("wykluczone") != 1){
+entry3.set("h",1)
+entries.reverse()[2].show()
+}
+else if (entry4 != null && entry4.field("h") == 0 && entry4.field("wykluczone") != 1){
+entry4.set("h",1)
+entries.reverse()[3].show()
+}
+else if (entry5 != null && entry5.field("h") == 0 && entry5.field("wykluczone") != 1){
+entry5.set('h',1)
+entries.reverse()[4].show()
+}
+else if (entry6 != null && entry6.field("h") == 0 && entry6.field("wykluczone") != 1){
+entry6.set('h',1)
+entries.reverse()[5].show()
+}
+else if (entry7 != null && entry7.field("h") == 0 && entry7.field("wykluczone") != 1){
+entry7.set('h',1)
+entries.reverse()[6].show()
+}
+else if (entry8 != null && entry8.field("h") == 0 && entry8.field("wykluczone") != 1){
+entry8.set('h',1)
+entries.reverse()[7].show()
+}
+else if (entry9 != null && entry9.field("h") == 0 && entry9.field("wykluczone") != 1){
+entry9.set('h',1)
+entries.reverse()[8].show()
+}
+else if (entry10 != null && entry10.field("h") == 0 && entry10.field("wykluczone") != 1){
+entry10.set('h',1)
+entries.reverse()[9].show()
+}
+else if (entry11 != null && entry11.field("h") == 0 && entry11.field("wykluczone") != 1){
+entry11.set('h',1)
+entries.reverse()[10].show()
+}
+else if (entry12 != null && entry12.field("h") == 0 && entry12.field("wykluczone") != 1){
+entry12.set('h',1)
+entries.reverse()[11].show()
+}
+else if (entry13 != null && entry13.field("h") == 0 && entry13.field("wykluczone") != 1){
+entry13.set('h',1)
+entries.reverse()[12].show()
+}
+else if (entry14 != null && entry14.field("h") == 0 && entry14.field("wykluczone") != 1){
+entry14.set('h',1)
+entries.reverse()[13].show()
+}
+else if (entry15 != null && entry15.field("h") == 0 && entry15.field("wykluczone") != 1){
+entry15.set('h',1)
+entries.reverse()[14].show()
+}
+else if (entry16 != null && entry16.field("h") == 0 && entry16.field("wykluczone") != 1){
+entry16.set('h',1)
+entries.reverse()[15].show()
+}
+else if (entry17 != null && entry17.field("h") == 0 && entry17.field("wykluczone") != 1){
+entry17.set('h',1)
+entries.reverse()[16].show()
+}
+else if (entry18 != null && entry18.field("h") == 0 && entry18.field("wykluczone") != 1){
+entry18.set('h',1)
+entries.reverse()[17].show()
+}
+else if (entry19 != null && entry19.field("h") == 0 && entry19.field("wykluczone") != 1){
+entry19.set('h',1)
+entries.reverse()[18].show()
+}
+else if (entry20 != null && entry20.field("h") == 0 && entry20.field("wykluczone") != 1){
+entry20.set('h',1)
+entries.reverse()[19].show()
+}
+else if (entry21 != null && entry21.field("h") == 0 && entry21.field("wykluczone") != 1){
+entry21.set('h',1)
+entries.reverse()[20].show()
+}
+else if (entry22 != null && entry22.field("h") == 0 && entry22.field("wykluczone") != 1){
+entry22.set('h',1)
+entries.reverse()[21].show()
+}
+else if (entry23 != null && entry23.field("h") == 0 && entry23.field("wykluczone") != 1){
+entry23.set('h',1)
+entries.reverse()[22].show()
+}
+else if (entry24 != null && entry24.field("h") == 0 && entry24.field("wykluczone") != 1){
+entry24.set('h',1)
+entries.reverse()[23].show()
+}
+else if (entry25 != null && entry25.field("h") == 0 && entry25.field("wykluczone") != 1){
+entry25.set('h',1)
+entries.reverse()[24].show()
+}
+else if (entry26 != null && entry26.field("h") == 0 && entry26.field("wykluczone") != 1){
+entry26.set('h',1)
+entries.reverse()[25].show()
+}
+else if (entry27 != null && entry27.field("h") == 0 && entry27.field("wykluczone") != 1){
+entry27.set('h',1)
+entries.reverse()[26].show()
+}
+else if (entry28 != null && entry28.field("h") == 0 && entry28.field("wykluczone") != 1){
+entry28.set('h',1)
+entries.reverse()[27].show()
+}
+else if (entry29 != null && entry29.field("h") == 0 && entry29.field("wykluczone") != 1){
+entry29.set('h',1)
+entries.reverse()[28].show()
+}
+else if (entry30 != null && entry30.field("h") == 0 && entry30.field("wykluczone") != 1){
+entry30.set('h',1)
+entries.reverse()[29].show()
+}
+else if (entry31 != null && entry31.field("h") == 0 && entry31.field("wykluczone") != 1){
+entry31.set('h',1)
+entries.reverse()[30].show()
+}
+else if (entry32 != null && entry32.field("h") == 0 && entry32.field("wykluczone") != 1){
+entry32.set('h',1)
+entries.reverse()[31].show()
+}
+else if (entry33 != null && entry33.field("h") == 0 && entry33.field("wykluczone") != 1){
+entry33.set('h',1)
+entries.reverse()[32].show()
+}
+else if (entry34 != null && entry34.field("h") == 0 && entry34.field("wykluczone") != 1){
+entry34.set('h',1)
+entries.reverse()[33].show()
+}
+else if (entry35 != null && entry35.field("h") == 0 && entry35.field("wykluczone") != 1){
+entry35.set('h',1)
+entries.reverse()[34].show()
+}
+else if (entry36 != null && entry36.field("h") == 0 && entry36.field("wykluczone") != 1){
+entry36.set('h',1)
+entries.reverse()[35].show()
+}
+else if (entry37 != null && entry37.field("h") == 0 && entry37.field("wykluczone") != 1){
+entry37.set('h',1)
+entries.reverse()[36].show()
+}
+else if (entry38 != null && entry38.field("h") == 0 && entry38.field("wykluczone") != 1){
+entry38.set('h',1)
+entries.reverse()[37].show()
+}
+else if (entry39 != null && entry39.field("h") == 0 && entry39.field("wykluczone") != 1){
+entry39.set('h',1)
+entries.reverse()[38].show()
+}
+else if (entry40 != null && entry40.field("h") == 0 && entry40.field("wykluczone") != 1){
+entry40.set('h',1)
+entries.reverse()[39].show()
+}
+else if (entry41 != null && entry41.field("h") == 0 && entry41.field("wykluczone") != 1){
+entry41.set('h',1)
+entries.reverse()[40].show()
+}
+else if (entry42 != null && entry42.field("h") == 0 && entry42.field("wykluczone") != 1){
+entry42.set('h',1)
+entries.reverse()[41].show()
+}
+else if (entry43 != null && entry43.field("h") == 0 && entry43.field("wykluczone") != 1){
+entry43.set('h',1)
+entries.reverse()[42].show()
+}
+else if (entry44 != null && entry44.field("h") == 0 && entry44.field("wykluczone") != 1){
+entry44.set('h',1)
+entries.reverse()[43].show()
+}
+else if (entry45 != null && entry45.field("h") == 0 && entry45.field("wykluczone") != 1){
+entry45.set('h',1)
+entries.reverse()[44].show()
+}
+else if (entry46 != null && entry46.field("h") == 0 && entry46.field("wykluczone") != 1){
+entry46.set('h',1)
+entries.reverse()[45].show()
+}
+else if (entry47 != null && entry47.field("h") == 0 && entry47.field("wykluczone") != 1){
+entry47.set('h',1)
+entries.reverse()[46].show()
+}
+else if (entry48 != null && entry48.field("h") == 0 && entry48.field("wykluczone") != 1){
+entry48.set('h',1)
+entries.reverse()[47].show()
+}
+else if (entry49 != null && entry49.field("h") == 0 && entry49.field("wykluczone") != 1){
+entry49.set('h',1)
+entries.reverse()[48].show()
+}
+else{
+lib().show();
+}
+
+}
+
+
+
