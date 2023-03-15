989effbd6056ba921d294196c0bc5adc6641ba96
wojtish <20014605+wojtish@users.noreply.github.com>
2023-03-15 15:05:05 +0100

    Update Biblioteki_shared.js

--- Biblioteki_shared.js
+++ Biblioteki_shared.js
@@ -1,3 +1,5 @@
+// Bilioteki shared skrypt
+
 var linkt = "tasker://secondary?text="
 var intview = intent("android.intent.action.VIEW");
 
@@ -6,109 +8,161 @@ if ( t == true )
 {
 intview.data(linkt + "memento")
 intview.send()
-//message("test ok");
-};
-};
+//message("test ok"); }; };
 
-
-var im = "important";
-var imbn = libByName(im);
-var ta = "trasy szlaki aktywności sportowe memento";
-var tabn = libByName(ta);
-var bm = "bieganie memento";
-var bmbn = libByName(bm);
-var pg = "pogoda";
-var pgbn = libByName(pg);
-var hp = "Haczów przestrzenie"
-var hpbn = libByName(hp)
-var zt = "zadania terminarz zajęć prac zadań zadania"
-var ztbn = libByName(zt)
-var hk = "Haczów katalog"
-var hkbn = libByName(hk)
-var ma = "metalowe artykuły"
-var mabn = libByName(ma)
-var tc = "techniczna"
-var tcbn = libByName(tc)
-var ps = "PRODUKTY SPOŻYWCZE"
-var psbn = libByName(ps)
-var rt = "RUTYNY"
-var rtbn = libByName(rt)
-var an = "angielski"
-var anbn = libByName(an)
-var lw = "ludzie w miłości kontaktujący się"
-var lwbn = libByName(lw)
-var pr  = "przepisy"
-var prbn  = libByName(pr)
-var lk = "lektury"
-var lkbn  = libByName(lk)
-var sm = "samochód"
-var smbn = libByName(sm)
-var sc = "sukcesy cuda"
-var scbn = libByName(sc)
-var gc = "garderoba ubrania ciuchy"
-var gcbn = libByName(gc)
-var pp = "plan posiłków"
-var ppbn = libByName(pp)
-var bw = "budżet wydatki przychody zarządzanie"
-var bwbn = libByName(bw)
-var cf = "ćwiczenia fitness youtube musepic"
-var cfbn = libByName(cf)
+//****A****
+//****A****
+//****A****
 var ag = "agenda memento harmonogram terminarz kalendarz zadania"
 var agbn = libByName(ag)
-var zb = "życie ukryte w Bogu Ojcu"
-var zbbn =libByName(zb)
-var mo = "mój ogród"
-var mobn = libByName(mo)
-var tm = "tabliczka mnożenia"
-var tmbn = libByName(tm)
-var tt = "time tracking"
-var ttbn = libByName(tt)
-var lj = "lokalizacje adresy"
-var ljbn = libByName(lj)
-var dz = "dziennik"
-var dzbn = libByName(dz)
-var rk = "Rzeszów katalog"
-var rkbn = libByName(rk)
-var wb = "waga baza danych"
-var wbbn = libByName(wb)
-var ic = "informacje"
-var icbn = libByName(ic)
-var sp = "SPOTKANIA"
-var spbn = libByName(sp)
-var pt = "programy telewizyjne"
-var ptbn = libByName(pt)
-var kk = "kody kreskowe"
-var kkbn = libByName(kk)
-var mm = "muzo muzyka"
-var mmbn = libByName(mm)
-var tk = "teorie kognitywne"
-var tkbn = libByName(tk)
-var sl = "stan liczników waga kilometry"
-var slbn = libByName(sl)
-var sb = "Słowo Boże"
-var sbbn = libByName(sb)
-var sj = "skrypty java script javascript memento";
-var sjbn = libByName(sj);
-var nm = "nauka memento"
-var nmbn = libByName(nm)
+var an = "angielski"
+var anbn = libByName(an);
+//****B****
+//****B****
+//****B****
+var bm = "bieganie memento";
+var bmbn = libByName(bm);
+var bw = "budżet wydatki przychody zarządzanie";
+var bwbn = libByName(bw);
+//****C****
+//****C****
+//****C****
+var cf = "ćwiczenia fitness youtube musepic";
+var cfbn = libByName(cf);
+//****D****
+//****D****
+//****D****
+var dz = "dziennik";
+var dzbn = libByName(dz);
+var dm = "dochodzenia memento investigations"
+var dmbn = libByName(dm)
+//****G****
+//****G****
+//****G****
+var gc = "garderoba ubrania ciuchy"
+var gcbn = libByName(gc)
+//****H****
+//****H****
+//****H****
+var hk = "Haczów katalog";
+var hkbn = libByName(hk);
+var hp = "Haczów przestrzenie";
+var hpbn = libByName(hp);
+//****I****
+//****I****
+//****I****
+var ic = "informacje";
+var icbn = libByName(ic);
+var im = "important";
+var imbn = libByName(im);
+//****J****
+//****J****
+//****J****
+
+//****K****
+//****K****
+//****K****
+var kk = "kody kreskowe";
+var kkbn = libByName(kk);
+//****L****
+//****L****
+//****L****
 var ll = "linki linki linki internet"
 var llbn = libByName(ll)
-var wi = "wiara"
-
-var wibn = libByName(wi)
-
-var wh = "wehikuły auta samochody memento"
-var whbn = libByName(wh)
-var wr = "wiersze"
-var wrbn = libByName(wr)
+var lj = "lokalizacje adresy";
+var ljbn = libByName(lj);
+var lk = "lektury";
+var lkbn  = libByName(lk);
+var lw = "ludzie w miłości kontaktujący się"
+var lwbn = libByName(lw)
+//****M****
+//****M****
+//****M****
+var ma = "metalowe artykuły";
+var mabn = libByName(ma);
+var mo = "mój ogród";
+var mobn = libByName(mo);
+var mm = "muzo muzyka";
+var mmbn = libByName(mm);
+//****N****
+//****N****
+//****N****
+var nm = "nauka memento"
+var nmbn = libByName(nm)
+//****P****
+//****P****
+//****P****
+var pp = "plan posiłków";
+var ppbn = libByName(pp);
+var pg = "pogoda";
+var pgbn = libByName(pg);
+var ps = "PRODUKTY SPOŻYWCZE";
+var psbn = libByName(ps);
+var pr  = "przepisy";
+var prbn  = libByName(pr);
+var pt = "programy telewizyjne";
+var ptbn = libByName(pt);
+//****R****
+//****R****
+//****R****
+var rp = "rower parts memento części";
+var rpbn = libByName(rp);
+var rt = "RUTYNY"
+var rtbn = libByName(rt)
+var rk = "Rzeszów katalog";
+var rkbn = libByName(rk);
+//****S****
+//****S****
+//****S****
+var sm = "samochód";
+var smbn = libByName(sm);
+var sj = "skrypty java script javascript memento"
+var sjbn = libByName(sj);
+var sb = "Słowo Boże";
+var sbbn = libByName(sb);
+var sp = "SPOTKANIA";
+var spbn = libByName(sp);
+var sl = "stan liczników waga kilometry";
+var slbn = libByName(sl);
+var sc = "sukcesy cuda";
+var scbn = libByName(sc);
+//****T****
+//****T****
+//****T****
+var tm = "tabliczka mnożenia"
+var tmbn = libByName(tm)
+var tc = "techniczna"
+var tcbn = libByName(tc);
+var te = "TEMPLATE TIMETUNE"
+var tebn = libByName(te)
+var tk = "teorie kognitywne"
+var tkbn = libByName(tk)
+var tt = "time tracking"
+var ttbn = libByName(tt);
+var ta = "trasy szlaki aktywności sportowe memento";
+var tabn = libByName(ta);
+//****W****
+//****W****
+//****W****
+var wb = "waga baza danych";
+var wbbn = libByName(wb);
 var lo = "ważne działania moje kroki log systemu"
 var lobn = libByName(lo)
+var wh = "wehikuły auta samochody memento"
+var whbn = libByName(wh)
+
+var wi = "wiara";
+var wibn = libByName(wi);
+var wrbn = libByName(wr);
+var wr = "wiersze";
+var wrbn = libByName(wr)
+//****Z****
+//****Z****
+//****Z****
+var zt = "zadania terminarz zajęć prac zadań zadania"
+var ztbn = libByName(zt)
 var zp = "za i przeciw analiza rozeznanie memento"
 var zpbn = libByName(zp)
 
-
-var te = "TEMPLATE TIMETUNE"
-var tebn = libByName(te)
-var dm = "dochodzenia memento investigations"
-var dmbn = libByName(dm)
-
+var zb = "życie ukryte w Bogu Ojcu"
+var zbbn =libByName(zb)
